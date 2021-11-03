7e45db55a60cde630294b2103aa9fb32c5cc5b7b
GDSGameOver <66680462+GDSGameOver@users.noreply.github.com>
2021-11-03 16:45:50 +0300

    init

---
+++ Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlatformSegment segment))
+        {
+            other.GetComponentInParent<Platform>().Break();
+        } 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out FinishPlatformSegment finishSegment))
+        {
+            collision.gameObject.GetComponentInParent<FinishPlatform>().Finish();
+        }
+    }
+}
---
+++ Assets/Scripts/Platform/FinishPlatform.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishPlatform : Platform
+{
+    public void Finish()
+    {
+        Debug.Log("Игра окончена");
+    }
+}
